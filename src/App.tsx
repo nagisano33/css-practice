981391b0a806b3405b8b2c8cf914f26e4cb7e861
nagisano33 <vita.stars2@gmail.com>
2024-07-28 01:36:04 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import { center } from "../styled-system/patterns";
+import { css } from "../styled-system/css/css";
+import { stack } from "../styled-system/patterns";
 import { Card } from "./Card/Card";
 
 function App() {
   return (
-    <div className={center({ height: "100vh" })}>
-      <Card />
+    <div
+      className={stack({
+        width: "100%",
+        alignItems: "center",
+        background: "bg"
+      })}
+    >
+      <div className={css({ height: "80px", color: "text.title", background: "bg.title", fontWeight: "title" })}>
+        <header>ヘッダー</header>
+      </div>
+      <div
+        className={css({
+          display: "flex",
+          justifyContent: "center",
+          gap: 8,
+          flexWrap: "wrap",
+        })}
+      >
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
