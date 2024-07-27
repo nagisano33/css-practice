04275b3753db6f6ba2858e558468a1f20ff65f1a
nagisano33 <vita.stars2@gmail.com>
2024-07-28 01:35:29 +0900

    Add components

--- src/Card/components/UserName.tsx
+++ src/Card/components/UserName.tsx
@@ -4,7 +4,7 @@ export function UserName() {
   return (
     <div>
       <h2 className={css({ fontWeight: 600 })}>なぎさの</h2>
-      <p className={css({ color: "#7d7d7d" })}>@nagisa_no33</p>
+      <p className={css({ color: "gray.weak" })}>@nagisa_no33</p>
     </div>
   );
 }
