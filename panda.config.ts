8c35b415466c261926fc89fec34b5248b49169fc
nagisano33 <vita.stars2@gmail.com>
2024-07-28 01:35:51 +0900

    Update panda.config.ts デザイントークン定義

--- panda.config.ts
+++ panda.config.ts
@@ -16,7 +16,48 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          gray: {
+            strong: { value: "#1A1A1A" },
+            DEFAULT: { value: "#4D4D4D" },
+            weak: { value: "#767676" },
+            weaker: { value: "#999999" },
+            weakest: { value: "#F2F2F2" },
+          },
+          blue: {
+            strong: { value: "#0D17C1" },
+            DEFAULT: { value: "#264AF4" },
+            weak: { value: "#7096F8" },
+            weakest: { value: "#E8F1FE" },
+          },
+        },
+
+        fontWeights: {
+          DEFAULT: { value: 400 },
+          medium: { value: 500 },
+          bold: { value: 700 },
+        },
+      },
+
+      semanticTokens: {
+        colors: {
+          bg: {
+            DEFAULT: { value: "{colors.gray.weakest}" },
+            title: { value: "{colors.blue}" },
+          },
+          text: {
+            DEFAULT: { value: "{colors.gray.strong}" },
+            title: { value: "{colors.blue.weakest}" },
+          },
+        },
+
+        fontWeights: {
+          title: { value: "{fontWeights.bold}" }
+        }
+      },
+    },
   },
 
   // The output directory for your css system
